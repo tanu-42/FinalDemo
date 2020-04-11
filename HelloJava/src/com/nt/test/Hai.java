03a6cf2e3434433bb01bf60f8c4b9b01b7e29b7e
TL <TL@gmail.com>
2020-04-11 21:45:59 +0530

    add one file

---
+++ HelloJava/src/com/nt/test/Hai.java
@@ -0,0 +1,11 @@
+package com.nt.test;
+
+public class Hai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is the another example");
+	}
+
+}
