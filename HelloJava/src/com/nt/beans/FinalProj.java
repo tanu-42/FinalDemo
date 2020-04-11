e7a1afdc37e6d66068cc609cf6912b32b0248ddd
TL <TL@gmail.com>
2020-04-11 21:39:01 +0530

    create proj

---
+++ HelloJava/src/com/nt/beans/FinalProj.java
@@ -0,0 +1,11 @@
+package com.nt.beans;
+
+public class FinalProj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello this is the tanoj kumar");
+	}
+
+}
